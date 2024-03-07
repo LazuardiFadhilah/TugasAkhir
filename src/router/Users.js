19dd4b7d52e259eca652f0883b33d7edd45b89bf
Lazuardi Fadhilah <Lazuardifadhillah@gmail.com>
2024-03-07 22:11:06 +0700

    sync data 7 mar 2024

--- src/router/Users.js
+++ src/router/Users.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controller/userController');
+const userController = require('../controller/userController.js');
 
 
-router.get('/', userController.show);
+
+router.get('/',userController.show);
 router.get('/:id', userController.showId);
-router.post('/register', userController.store);
+router.post('/register',userController.store);
 router.put('/:id', userController.update);
 
 module.exports = router;
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const user = require('./Users');
 
+
 router.use('/users', user);
+
 
 module.exports = router;

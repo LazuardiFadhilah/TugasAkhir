a09670b12889f845a18f39f9e5b952c650d98835
Lazuardi Fadhilah <Lazuardifadhillah@gmail.com>
2024-03-13 21:05:23 +0700

    add relation on db post with files

---
+++ src/migrations/20240313140350-relation-post-to-file.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.changeColumn('Posts', 'thumbnail', {
+      allowNull: true,
+      references:{
+        model:'Files',
+        key:'id',
+      },
+      type: Sequelize.UUID,
+      onUpdate: 'CASCADE',
+      onDelete: 'SET NULL'
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};

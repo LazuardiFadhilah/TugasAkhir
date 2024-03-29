f510a76d112ef0cf7e8f24658ebfb511caf5d1ae
Lazuardi Fadhilah <Lazuardifadhillah@gmail.com>
2024-03-29 15:14:31 +0700

    add confirmation email

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,161 +1,169 @@
-const { Users } = require('../models');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const env = require('dotenv').config().parsed;
+const { Users } = require("../models");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const env = require("dotenv").config().parsed;
+const SendEmailController = require("../helper/sendEmail"); 
 
 class AuthController {
-    generateToken = async (payload) => {
-        const expiresIn = env.JWT_ACCESS_TOKEN_EXPIRES_IN;
-        const accessToken = jwt.sign(
-            { userId: payload.id, },
-            env.JWT_ACCESS_TOKEN_SECRET,
-            { expiresIn: env.JWT_ACCESS_TOKEN_EXPIRES_IN }
-        );
+  generateToken = async (payload) => {
+    const expiresIn = env.JWT_ACCESS_TOKEN_EXPIRES_IN;
+    const accessToken = jwt.sign(
+      { userId: payload.id },
+      env.JWT_ACCESS_TOKEN_SECRET,
+      { expiresIn: env.JWT_ACCESS_TOKEN_EXPIRES_IN }
+    );
 
-        const refreshToken = jwt.sign(
-            { userId: payload.id },
-            env.JWT_REFRESH_TOKEN_SECRET,
-            { expiresIn: env.JWT_REFRESH_TOKEN_EXPIRES_IN }
-        );
+    const refreshToken = jwt.sign(
+      { userId: payload.id },
+      env.JWT_REFRESH_TOKEN_SECRET,
+      { expiresIn: env.JWT_REFRESH_TOKEN_EXPIRES_IN }
+    );
 
-        return { accessToken, refreshToken, expiresIn };
-    };
+    return { accessToken, refreshToken, expiresIn };
+  };
 
-    login = async (req, res) => {
-        try {
-            const user = await Users.findOne({
-                where: {
-                    email: req.body.email,
-                },
-        
-            });
+  login = async (req, res) => {
+    try {
+      const user = await Users.findOne({
+        where: {
+          email: req.body.email,
+        },
+      });
 
-            // check if user not found
-            if (!user) {
-                return res.status(400).json({ message: "USER_NOT_FOUND" });
-            }
-
-            // check if password is valid
-            const isPasswordValid = bcrypt.compareSync(req.body.password, user.password);
-
-            if (!isPasswordValid) {
-                return res.status(400).json({ message: "INVALID_PASSWORD" });
-            }
-
-            // generate Token
-            const { accessToken, expiresIn, refreshToken } = await this.generateToken(user);
-
-
-            // parse expiresIn from minute, hour, daily into milisecond
-            //    example 15m, 1h, 1d
-            const expiresInMilisecond = expiresIn.slice(0, expiresIn.length - 1);
-            const expiresInType = expiresIn.slice(expiresIn.length - 1);
-            let expiresInInMilisecond = 0;
-            if (expiresInType === "m") {
-                expiresInInMilisecond = expiresInMilisecond * 60000;
-            } else if (expiresInType === "h") {
-                expiresInInMilisecond = expiresInMilisecond * 3600000;
-            } else if (expiresInType === "d") {
-                expiresInInMilisecond = expiresInMilisecond * 86400000;
-            }
-
-            console.log(expiresInInMilisecond);
-
-            
-            return res.status(200).json({
-                code: 200,
-                message: "Berhasil Masuk",
-                accessToken: accessToken,
-                refreshToken: refreshToken,
-                expiresIn: expiresInInMilisecond,
-                tokenType: "Bearer",
-                user: {
-                    id: user.id,
-                    fullName: user.fullName,
-                    email: user.email,
-                    role: user.role,
-                    status: user.status,
-                    avatar: user.avatar,
-                    createdAt: user.createdAt,
-                    updatedAt: user.updatedAt,
-                    deletedAt: user.deletedAt,
-                },
-            });
-
-        } catch (error) {
-            return res.status(400).json({ message: error.message });
-        }
-    }
-
-    register = async (req, res) => {
-        try {
-            const { fullName, email, password, role} = req.body;
-
-            // check if email already exist
-            const isEmailExist = await Users.findOne({ where: { email: email } });
-            if (isEmailExist) {
-                return res.status(400).json({ message: "EMAIL_ALREADY_EXIST" });
-            }
-
-            // check role sudah sesuai apa belum
-            const isRoleValid = role === "Creator" || role === "Super Admin";
-            if (!isRoleValid) {
-                return res.status(400).json({ message: "Role must be Creator or Super Admin" });
-            }
-
-            // // check status valid
-            // const isStatusValid = status === "Active" || status === "Suspend";
-            // if (!isStatusValid) {
-            //     return res.status(400).json({ message: "STATUS_INVALID" });
-            // }
-
-            // create user
-            const user = await Users.create({
-                fullName: fullName,
-                email: email,
-                password: bcrypt.hashSync(password, 10),
-                role: role,
-                status: "Active",
-                avatar: null,
-            });
-            return res.json({
-                "code": 201,
-                "message": `Data berhasil dibuat`,
-                "data": user,
-            })
-        } catch (error) {
-            return res.status(400).json({ message: error.message });
-        }
-    }
-
-    refreshToken = async (req,res)=>{
-        const refreshToken = req.body.refreshToken;
-        if(!refreshToken){
-          res.status(401).json({message:"REFRESH_TOKEN_REQUIRED"});
-        }
-      
-        const verified = jwt.verify(refreshToken,env.JWT_REFRESH_TOKEN_SECRET);
-       
-        try {
-          if(!verified){
-            throw { message:"UNAUTHORIZED" };
-          }
-          const userId=verified.userId;
-          const user = await Users.findOne({where:{id:userId}});
-          if(!user){
-            throw { message:"USER_NOT_FOUND" };
-          }
-          const { accessToken, refreshToken } = await this.generateToken(user);
-          return res.status(200).json({accessToken,refreshToken});
-        }catch(e){
-          if (e.message == "invalid token") {
-            e.message = "INVALID_TOKEN";
-          } else if (e.message == "jwt expired") {
-            e.message = "TOKEN_EXPIRED";
-          }
-          res.status(401).json({message:e.message});
-        }
+      // check if user not found
+      if (!user) {
+        return res.status(400).json({ message: "USER_NOT_FOUND" });
       }
+
+      // check if password is valid
+      const isPasswordValid = bcrypt.compareSync(
+        req.body.password,
+        user.password
+      );
+
+      if (!isPasswordValid) {
+        return res.status(400).json({ message: "INVALID_PASSWORD" });
+      }
+
+      // generate Token
+      const { accessToken, expiresIn, refreshToken } = await this.generateToken(
+        user
+      );
+
+      // parse expiresIn from minute, hour, daily into milisecond
+      //    example 15m, 1h, 1d
+      const expiresInMilisecond = expiresIn.slice(0, expiresIn.length - 1);
+      const expiresInType = expiresIn.slice(expiresIn.length - 1);
+      let expiresInInMilisecond = 0;
+      if (expiresInType === "m") {
+        expiresInInMilisecond = expiresInMilisecond * 60000;
+      } else if (expiresInType === "h") {
+        expiresInInMilisecond = expiresInMilisecond * 3600000;
+      } else if (expiresInType === "d") {
+        expiresInInMilisecond = expiresInMilisecond * 86400000;
+      }
+
+      console.log(expiresInInMilisecond);
+
+      return res.status(200).json({
+        code: 200,
+        message: "Berhasil Masuk",
+        accessToken: accessToken,
+        refreshToken: refreshToken,
+        expiresIn: expiresInInMilisecond,
+        tokenType: "Bearer",
+        user: {
+          id: user.id,
+          fullName: user.fullName,
+          email: user.email,
+          role: user.role,
+          status: user.status,
+          avatar: user.avatar,
+          createdAt: user.createdAt,
+          updatedAt: user.updatedAt,
+          deletedAt: user.deletedAt,
+        },
+      });
+    } catch (error) {
+      return res.status(400).json({ message: error.message });
+    }
+  };
+
+  register = async (req, res) => {
+    try {
+      const { fullName, email, password, role } = req.body;
+
+      // check if email already exist
+      const isEmailExist = await Users.findOne({ where: { email: email } });
+      if (isEmailExist) {
+        return res.status(400).json({ message: "EMAIL_ALREADY_EXIST" });
+      }
+
+      // check role sudah sesuai apa belum
+      const isRoleValid = role === "Creator" || role === "Super Admin";
+      if (!isRoleValid) {
+        return res
+          .status(400)
+          .json({ message: "Role must be Creator or Super Admin" });
+      }
+
+      // // check status valid
+      // const isStatusValid = status === "Active" || status === "Suspend";
+      // if (!isStatusValid) {
+      //     return res.status(400).json({ message: "STATUS_INVALID" });
+      // }
+
+      // create user
+      const user = await Users.create({
+        fullName: fullName,
+        email: email,
+        password: bcrypt.hashSync(password, 10),
+        role: role,
+        status: "Active",
+        avatar: null,
+      });
+
+    //  confirm email
+      SendEmailController.sendEmailRegister(user.email, user.fullName);
+
+      return res.json({
+        code: 201,
+        message: `Data berhasil dibuat`,
+        data: user,
+      });
+    } catch (error) {
+      return res.status(400).json({ message: error.message });
+    }
+  };
+
+  refreshToken = async (req, res) => {
+    const refreshToken = req.body.refreshToken;
+    if (!refreshToken) {
+      res.status(401).json({ message: "REFRESH_TOKEN_REQUIRED" });
+    }
+
+    const verified = jwt.verify(refreshToken, env.JWT_REFRESH_TOKEN_SECRET);
+
+    try {
+      if (!verified) {
+        throw { message: "UNAUTHORIZED" };
+      }
+      const userId = verified.userId;
+      const user = await Users.findOne({ where: { id: userId } });
+      if (!user) {
+        throw { message: "USER_NOT_FOUND" };
+      }
+      const { accessToken, refreshToken } = await this.generateToken(user);
+      return res.status(200).json({ accessToken, refreshToken });
+    } catch (e) {
+      if (e.message == "invalid token") {
+        e.message = "INVALID_TOKEN";
+      } else if (e.message == "jwt expired") {
+        e.message = "TOKEN_EXPIRED";
+      }
+      res.status(401).json({ message: e.message });
+    }
+  };
 }
 
 module.exports = new AuthController();
--- src/helper/sendEmail.js
+++ src/helper/sendEmail.js
@@ -1,40 +1,90 @@
-const express = require('express');
-const nodemailer = require('nodemailer');
-const app = express();
-const dotenv = require('dotenv').config().parsed;
+const express = require("express");
+const nodemailer = require("nodemailer");
+const dotenv = require("dotenv").config().parsed;
+const handlebars = require("handlebars");
+const fs = require("fs");
+const path = require('path');
+const { throws } = require("assert");
+
+class SendEmailController {
+  sendEmailRegister = (to, username) => {
+    const source = fs.readFileSync(path.resolve(__dirname, './emailTemplate.html'), 'utf-8').toString();
+    const template = handlebars.compile(source);
+    const replacement = {
+      username:username,
+    }
+    const htmlToSend = template(replacement);
+    const transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      },
+    });
+
+  
+    // const { to, subject, text } = req.body;
+
+    // if (!to || !subject || !text) {
+    //   return res
+    //     .status(400)
+    //     .send("Missing required fields: to, subject, or text!");
+    // }
+
+    const mailOptions = {
+      from: process.env.SMTP_USER,
+      to,
+      subject:'Register new Account',
+      // text,
+      html: htmlToSend,
 
 
-class SendEmailController{
-      
-       sendEmail = async (req, res) => {
-        const transporter = nodemailer.createTransport({
-            host: process.env.SMTP_HOST,
-            port: process.env.SMTP_PORT,
-            auth: {
-              user: process.env.SMTP_USER,
-              pass: process.env.SMTP_PASS
-            }
-          });
-        const { to, subject, text } = req.body;
-      
-        if (!to || !subject || !text) {
-          return res.status(400).send('Missing required fields: to, subject, or text!');
-        }
-      
-        const mailOptions = {
-          from: process.env.SMTP_USER,
-          to,
-          subject,
-          text
-        };
-      
-        transporter.sendMail(mailOptions, (error, info) => {
-          if (error) {
-            return res.status(500).send('Error while sending email!');
-          }
-          res.send(`Email sent: ${info.response}`);
-        });
-      };     
-}
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        throw new Error('error');
+      }
+     return true;
+    });
+  };
+
+  sendEmail = async (req, res) => {
+    const transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      },
+    });
+
+  
+    const { to, subject, text } = req.body;
+
+    if (!to || !subject || !text) {
+      return res
+        .status(400)
+        .send("Missing required fields: to, subject, or text!");
+    }
+
+    const mailOptions = {
+      from: process.env.SMTP_USER,
+      to,
+      subject,
+      text,
+
+
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        return res.status(500).send("Error while sending email!");
+      }
+      res.send(`Email sent: ${info.response}`);
+    });
+  };
+  };
 
 module.exports = new SendEmailController();
